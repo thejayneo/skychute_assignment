2373c6b12bcb2b7bd60d6570c35a5b7156fab9b8
Jackson <cas022032@coderacademy.edu.au>
2021-02-22 22:27:04 +1100

    Initial commit and assignment 1 solution.

---
+++ src/assignment_01/stack.ts
@@ -0,0 +1,39 @@
+class Stack {
+    element: Array<Array<number>>;
+
+    constructor() {
+        this.element = [[]];
+    };
+
+    push(num: number) {
+        if (num < this.element[this.element.length - 1][1]) {
+            this.element[this.element.length] = [num, this.element[this.element.length - 1][1]];
+            return this.element.length;
+        } else {
+            this.element[this.element.length] = [num, num];
+            return this.element.length;
+        }
+    };
+
+    pop() {
+        console.log(this.element[this.element.length - 1][0]);
+        delete this.element[this.element.length - 1];
+        this.element.length = this.element.length - 1;
+    };
+
+    max() {
+        return this.element[this.element.length-1][1];
+    };
+};
+
+let test = new Stack;
+test.push(-5);
+test.push(1);
+test.push(7);
+test.push(3);
+console.log(test);
+
+test.pop();
+console.log(test);
+
+console.log(test.max())
