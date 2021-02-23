fad49679f78cf61120406f73251e4bfa70a1c29a
Jackson <cas022032@coderacademy.edu.au>
2021-02-23 11:31:27 +1100

    Added max array record.

--- src/assignment_01/queue.ts
+++ src/assignment_01/queue.ts
@@ -1,8 +1,10 @@
 class Queue {
     element: Array<Array<number>>;
+    maxArray: Array<number>;
 
     constructor() {
         this.element = [[]];
+        this.maxArray = [];
     };
 
     push(num: number) {
@@ -11,6 +13,7 @@ class Queue {
             return this.element.length;
         } else {
             this.element[this.element.length] = [num, num];
+            this.maxArray[this.maxArray.length] = num;
             return this.element.length;
         }
     };
@@ -20,7 +23,9 @@ class Queue {
         if (this.element[0][1] < this.element[this.element.length - 1][1]) {
             delete this.element[0];
         } else {
-            
+            delete this.maxArray[this.maxArray.length - 1];
+            this.maxArray.length = this.maxArray.length - 1;
+            this.element[this.element.length - 1][1] = this.maxArray[length - 1];
         };
     };
 
@@ -28,3 +33,18 @@ class Queue {
         return this.element[this.element.length-1][1];
     };
 };
+
+let test1 = new Queue;
+test1.push(-5);
+test1.push(1);
+test1.push(1);
+test1.push(1);
+test1.push(7);
+test1.push(7);
+test1.push(3);
+console.log(test1);
+
+test1.pop();
+console.log(test1);
+
+console.log(test1.max())
