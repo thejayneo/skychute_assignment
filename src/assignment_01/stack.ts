c4b4d70615a95102a7194a48adddfd407f83b86f
Jackson <cas022032@coderacademy.edu.au>
2021-02-23 20:48:07 +1100

    Tided up with TSLint enabled.

--- src/assignment_01/stack.ts
+++ src/assignment_01/stack.ts
@@ -1,12 +1,12 @@
 class Stack {
-    element: Array<Array<number>>;
+    element: number[][];
 
     constructor() {
         this.element = [];
     };
 
     push(num: number): number {
-        if (this.element.length == 0 || num > this.element[this.element.length - 1][1]) {
+        if (this.element.length === 0 || num > this.element[this.element.length - 1][1]) {
             this.element[this.element.length] = [num, num];
         } else {
             this.element[this.element.length] = [num, this.element[this.element.length - 1][1]];
@@ -15,7 +15,7 @@ class Stack {
     };
 
     pop(): number {
-        let lastElement: number = this.element[this.element.length - 1][0];
+        const lastElement: number = this.element[this.element.length - 1][0];
         delete this.element[this.element.length - 1];
         this.element.length = this.element.length - 1;
         return lastElement;
@@ -25,16 +25,3 @@ class Stack {
         return this.element[this.element.length-1][1];
     };
 };
-
-let test = new Stack;
-test.push(-5);
-test.push(1);
-test.push(7);
-test.push(3);
-console.log(test);
-
-test.pop();
-test.pop();
-console.log(test);
-
-console.log(test.max())
