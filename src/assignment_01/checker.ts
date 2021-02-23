8bbe4ea34ef188fb74a89cf3509bbb6c9112e93c
Jackson <cas022032@coderacademy.edu.au>
2021-02-23 14:55:25 +1100

    Initial commit for checker.

---
+++ src/assignment_01/checker.ts
@@ -0,0 +1,37 @@
+function checker(seq:string): string {
+    // Init. bracket coutners
+    let pO: number;
+    let pC: number;
+    let bO: number;
+    let bC: number;
+    let sO: number;
+    let sC: number;
+    // Convert string to array
+    const seqArr = seq.split('');
+    // Define function to increment counters
+    function assign(seqItem: string) {
+        if (seqItem == '(') {
+            pO += 1;
+        } else if (seqItem == ')') {
+            pC += 1;
+        } else if (seqItem == '{') {
+            bO += 1;
+        } else if (seqItem == '}') {
+            bC += 1;
+        } else if (seqItem == '[') {
+            sO += 1;
+        } else if (seqItem == ']') {
+            bC += 1;
+        };
+    }
+    // Iterate over array with pre-defined function
+    seqArr.forEach(assign)
+    // Check matching counters are equal
+    if (pO == pC && bO == bC && sO == sC) {
+        return 'Valid Sequence';
+    } else {
+        return 'Invalid Sequence';
+    };
+}
+
+checker(`(]})[{`);
