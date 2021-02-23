6229a91d8dab42ac13968128778c873e9ce8726c
Jackson <cas022032@coderacademy.edu.au>
2021-02-24 00:44:03 +1100

    Remade queue solution.

--- src/assignment_01/queue.ts
+++ src/assignment_01/queue.ts
@@ -1,49 +1,48 @@
 class Queue {
-    element: Array<Array<number>>;
-    maxArray: Array<number>;
+    // Initalize two dequeues
+    element: number[];
+    elementMaxes: number[];
+    maxLowPointer: number;
 
     constructor() {
         this.element = [];
-        this.maxArray = [];
+        this.elementMaxes = [];
+        this.maxLowPointer = 0;
     };
 
     push(num: number): number{
-        if (this.element.length == 0 || num > this.element[0][1]) {
-            this.element = [[num,num],...this.element];
-            if (this.maxArray.length == 0) {
-                this.maxArray[0] = num;
-            } else {
-                this.maxArray[this.maxArray.length] = num;
-            };
+        if (this.element.length === 0) {
+            this.element[0] = num;
+            this.elementMaxes[0] = num;
         } else {
-            this.element = [[num, this.element[0][1]],...this.element];
-        }
+            this.element = [num,...this.element];
+        // I do not like this 'while' it is not O(c) but I can't think of an O(c) solution. Sorry :'c
+            while (num > this.elementMaxes[this.maxLowPointer]) {
+                delete this.elementMaxes[this.maxLowPointer];
+                this.maxLowPointer += 1;
+            };
+            if (this.maxLowPointer === 0) {
+                this.elementMaxes = [num,...this.elementMaxes];
+            } else if (this.elementMaxes[this.maxLowPointer - 1] === undefined) {
+                this.elementMaxes[this.maxLowPointer - 1] = num;
+            };
+            this.maxLowPointer = 0;
+        };
         return this.element.length;
     };
 
-    pop(): number {
-        let queueFirst: number = this.element[this.element.length-1][0];
-        if (this.element[this.element.length-1][1] > this.element[0][1]) {
-            this.element[0][1] = this.maxArray[this.maxArray.length -1];
+    pop() {
+        if (this.element.length === 0) {
+            return
+        } else if (this.element[this.element.length - 1] === this.elementMaxes[this.elementMaxes.length - 1]) {
+            this.element.length -= 1;
+            this.elementMaxes.length -= 1;
+        } else {
+            this.element.length -=1;
         };
-        this.element.length -= 1;
-        return queueFirst;
     };
 
     max(): number {
-        return this.element[0][1];
+        return this.elementMaxes[this.elementMaxes.length - 1];
     };
 };
-
-let test1 = new Queue;
-test1.push(-5);
-test1.push(1);
-test1.push(7);
-test1.push(7);
-test1.push(3);
-console.log(test1);
-
-test1.pop();
-console.log(test1);
-
-console.log(test1.max())
